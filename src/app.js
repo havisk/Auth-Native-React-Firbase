1fcc8f9a432d6610ae284f4f5d683f66d8252239
Kool Havis <havisk@gmail.com>
2017-08-17 08:26:13 -0400

    added modules, linked common files to a indexjs

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
 import React, {Component} from 'react';
 import { View, Text} from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyAk_JBcPs-JzwtEvrJkDdd4_MIy1TLa1h8",
+            authDomain: "authentication-b5dcb.firebaseapp.com",
+            databaseURL: "https://authentication-b5dcb.firebaseio.com",
+            projectId: "authentication-b5dcb",
+            storageBucket: "authentication-b5dcb.appspot.com",
+            messagingSenderId: "466123149023"
+        });
+    }
     render() {
         return (
             <View>
+                <Header headerText="Authentication"/>
                 <Text>
                     An App
                 </Text>
             </View>
-        )
-
+        );
     }
-};
+}
 
 export default App;
